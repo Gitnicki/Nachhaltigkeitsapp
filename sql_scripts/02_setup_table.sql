1f005155a4176b8667ad5d4704a72fb6b7fcfd57
Marc E. <marc.ehrlich@docc.techstarter.de>
2024-01-03 16:07:38 +0100

    sql scrips created

---
+++ sql_scripts/02_setup_table.sql
@@ -0,0 +1,7 @@
+USE Co2;
+
+CREATE TABLE IF NOT EXISTS Abdruck (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    description VARCHAR(255) NOT NULL,
+    -- status VARCHAR(255) DEFAULT 'open'
+);
---
+++ sql_scripts/03_isert_data.sql
@@ -0,0 +1,5 @@
+USE Co2;
+
+INSERT INTO Abdruck(id, Name , Email ,Passwort) VALUES (value_1,value_2,value_3);
+
+
