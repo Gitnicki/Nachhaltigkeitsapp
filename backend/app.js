f307835127f823614abe6b18722aaca5ed316ab9
onben <ons.benyoussef12@outlook.com>
2024-01-03 10:21:46 +0100

    main.js and app.js

--- backend/app.js
+++ backend/app.js
@@ -3,17 +3,3 @@
 // var template = "{{name}} is {{age}} years old.";
 // var data = { name: "John", age: 30 };
 // var rendered = Mustache.render(template, data);
-
-window.onload = function () {
-    showPopup();
-}
-
-function showPopup() {
-    document.getElementById('overlay').style.display = 'block';
-    document.getElementById('popup').style.display = 'block';
-}
-
-function closePopup() {
-    document.getElementById('overlay').style.display = 'none';
-    document.getElementById('popup').style.display = 'none';
-}
