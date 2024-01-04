5b660b4ecf8dd92ecab3b223a6b623453deda47d
Marc E. <marc.ehrlich@docc.techstarter.de>
2024-01-04 16:22:12 +0100

    rechnung routes createt

--- frontend/main.js
+++ frontend/main.js
@@ -15,8 +15,27 @@ function closePopup() {
 // Annahme: Die spezifischen CO2-Emissionswerte können je nach Fahrzeugtyp und Kraftstoff variieren.
 // Hier werden allgemeine Durchschnittswerte verwendet.
 
+let verkehrsmittel = ""
+
+
+document.getElementById('auto').addEventListener('click', function () {
+    verkersmittel = "auto";
+})
+
+document.getElementById('öffentlich').addEventListener('click')
+document.getElementById('bus').addEventListener('click')
+document.getElementById('bahn').addEventListener('click')
+
+if (verkersmittel.includes("bus") && verkehrsmittel.includes("öffentlich")) {
+    verkersmittel = "bus";
+}
+if (verkersmittel.includes("bahn") && verkehrsmittel.includes("öffentlich")) {
+    verkersmittel = "bahn";
+}
+
+
 // Funktion zur Berechnung der CO2-Emissionen für die Auto-Nutzung
-function co2berechnung(verkehrsmittel, kraftstoff, verbrauchPro100km, jahreskilometer,maxico2) {
+function co2berechnung(verkehrsmittel, kraftstoff, verbrauchPro100km, jahreskilometer, maxico2) {
     let co2EmissionenProLiter; // CO2-Emissionen pro Liter Kraftstoff
     let verbrauchInLiter; // Verbrauch in Litern pro Jahr
     let co2Emissionen; // Gesamte CO2-Emissionen pro Jahr
@@ -25,7 +44,7 @@ function co2berechnung(verkehrsmittel, kraftstoff, verbrauchPro100km, jahreskilo
         if (kraftstoff === 'Benzin') {
             co2EmissionenProLiter = 2390; // Menge für Benzin in g CO2 pro Liter
         } else if (kraftstoff === 'Diesel') {
-            co2EmissionenProLiter = 2640 ; // Menge für Diesel in g CO2 pro Liter
+            co2EmissionenProLiter = 2640; // Menge für Diesel in g CO2 pro Liter
         } else if (kraftstoff === 'Hybrid') {
             co2EmissionenProLiter = 1111; // 
         } else if (kraftstoff === 'Elektro') {
@@ -36,14 +55,14 @@ function co2berechnung(verkehrsmittel, kraftstoff, verbrauchPro100km, jahreskilo
             co2EmissionenProLiter = 2666; // Menge für CNG in g CO2 pro Liter
         }
         // Berechnung des Verbrauchs in Litern pro Jahr
-    verbrauchInLiter = (verbrauchPro100km / 100) * jahreskilometer;
-     // Berechnung der Gesamten CO2-Emissionen pro Jahr
-     co2Emissionen = verbrauchInLiter * co2EmissionenProLiter;
+        verbrauchInLiter = (verbrauchPro100km / 100) * jahreskilometer;
+        // Berechnung der Gesamten CO2-Emissionen pro Jahr
+        co2Emissionen = verbrauchInLiter * co2EmissionenProLiter;
     } else if (verkehrsmittel === 'Bus') {
 
-       co2Emissionen= jahreskilometer * 23;
+        co2Emissionen = jahreskilometer * 23;
     } else if (verkehrsmittel === 'Bahn') {
-        co2Emissionen= jahreskilometer * 36
+        co2Emissionen = jahreskilometer * 36
     }
     return co2Emissionen;
 }
