1a435f674dc4c2c82f7734189d103c716156ebb1
Gitnicki <Richter.Nicole.D@gmail.com>
2024-01-05 16:35:08 +0100

    streaming und socialmedia fertig

--- frontend/freizeit.js
+++ frontend/freizeit.js
@@ -1,7 +1,7 @@
 // Funktion zur Berechnung der CO2-Emissionen für die Auto-Nutzung
-function co2berechnungperHour(gamingHours, dropdownvalue, platform, dropdownisstreaming) {
+function co2berechnungperHour(gamingHours, dropdownvalue, platform, dropdownisstreaming, streamingHours, dropdownissocialmedia, socialHours) {
   let co2EmissionenProStunde; // CO2-Emissionen pro Stunde
-  let co2Emissionen; // Gesamte CO2-Emissionen pro Woche
+  let co2Emissionen = 0; // Gesamte CO2-Emissionen pro Woche
     if (dropdownvalue === "Ja") {
       console.log("test");
       if (platform === 'standpc') {
@@ -17,15 +17,35 @@ function co2berechnungperHour(gamingHours, dropdownvalue, platform, dropdownisst
       } else if (platform === 'xbox') {
           co2EmissionenProStunde = 63; 
       }
-      if (dropdownisstreaming === "Ja") {
-
-      }
       console.log(gamingHours);
       console.log(co2EmissionenProStunde);
       // Berechnung der Gesamten CO2-Emissionen pro Woche
       co2Emissionen = gamingHours * co2EmissionenProStunde;
-      console.log(co2Emissionen);
+      if (dropdownisstreaming === "Ja") {
+        console.log(streamingHours);
+        co2Emissionen += 130 * streamingHours;
+      }
+      if (dropdownissocialmedia === "Ja") {
+        console.log(socialHours);
+        co2Emissionen += 138 * socialHours;
+      }
+  
     }
+    if (dropdownisstreaming === "Ja") {
+      console.log(streamingHours);
+      co2Emissionen += 130 * streamingHours;
+
+      if (dropdownissocialmedia === "Ja") {
+        console.log(socialHours);
+        co2Emissionen += 138 * socialHours;
+      }
+    }
+
+    if (dropdownissocialmedia === "Ja") {
+      console.log(socialHours);
+      co2Emissionen += 138 * socialHours;
+    }
+
       
   console.log(co2Emissionen);
   return co2Emissionen; // Brgrenze die Anzahl der Dezimalstellen
@@ -38,7 +58,10 @@ document.getElementById('button-berechnen').addEventListener('click', function (
   const platform = document.getElementById('platform').value;
   const gamingHours = parseInt(document.getElementById('gamingh').value);
   const dropdownisstreaming = document.getElementById('dropdownisstreaming').value;
-  const co2berechnung = co2berechnungperHour(gamingHours, dropdownvalue, platform, dropdownisstreaming);
+  const streamingHours = parseInt(document.getElementById('streamingh').value);
+  const dropdownissocialmedia = document.getElementById('dropdownissocialmedia').value;
+  const socialHours = parseInt(document.getElementById('socialh').value);
+  const co2berechnung = co2berechnungperHour(gamingHours, dropdownvalue, platform, dropdownisstreaming, streamingHours, dropdownissocialmedia, socialHours);
   console.log('Geschätzte CO2-Emissionen für die Gaming-Nutzung:', co2berechnung);
   });
 
