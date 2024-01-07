19fadc86a23426c0a17dee487b8b47db6acd2b1c
Gitnicki <Richter.Nicole.D@gmail.com>
2024-01-07 16:54:27 +0100

    tiere implementiert freizeit.js

--- frontend/freizeit.js
+++ frontend/freizeit.js
@@ -1,7 +1,8 @@
 // Funktion zur Berechnung der CO2-Emissionen für die Freizeit-Nutzung
-function co2berechnungperHour(gamingHours, dropdownvalue, platform, dropdownisstreaming, streamingHours, dropdownissocialmedia, socialHours, dropdowniszeitung, dropdowniszeitungdp) {
+function co2berechnungperHour(gamingHours, dropdownvalue, platform, dropdownisstreaming, streamingHours, dropdownissocialmedia, socialHours, dropdowniszeitung, dropdowniszeitungdp, dropdownistiere, dropdownwelchestier, dropdownanzahltiere) {
   let co2EmissionenProStunde; // CO2-Emissionen pro Stunde
   let co2EmissionenProNutzung; // CO2-Emissionen pro Nutzung der Zeitung
+  let co2EmissionenProTier; // Co2-Emissionen pro Tier auf ein Jahr
   let co2Emissionen = 0; // Gesamte CO2-Emissionen pro Woche
     if (dropdownvalue === "Ja") {
       if (platform === 'standpc') {
@@ -37,8 +38,22 @@ function co2berechnungperHour(gamingHours, dropdownvalue, platform, dropdownisst
       }
       co2Emissionen += co2EmissionenProNutzung * 7;
     }
-  
+      if (dropdownistiere === "Ja") {
+        if (dropdownwelchestier === 'Hund') {
+          co2EmissionenProTier = 725000; // Co2Emissionen in Gramm auf ein Jahr
+      } else if (dropdownwelchestier === 'Katze') {
+          co2EmissionenProTier = 400000; 
+      } else if (dropdownwelchestier === 'Kleintier') {
+          co2EmissionenProTier = 100000;  
+      } else if (dropdownwelchestier === 'Pferd') {
+          co2EmissionenProTier = 3100000;  
+      } else if (dropdownwelchestier === 'Vogel') {
+          co2EmissionenProTier = 25000; 
+      } 
+      co2Emissionen += co2EmissionenProTier * dropdownanzahltiere;
+    
     }
+  }   
     if (dropdownisstreaming === "Ja") {
       console.log(streamingHours);
       co2Emissionen += 130 * streamingHours;
@@ -55,6 +70,20 @@ function co2berechnungperHour(gamingHours, dropdownvalue, platform, dropdownisst
       }
       co2Emissionen += co2EmissionenProNutzung * 7;
     }
+    if (dropdownistiere === "Ja") {
+      if (dropdownwelchestier === 'Hund') {
+        co2EmissionenProTier = 725000; // Co2Emissionen in Gramm auf ein Jahr
+    } else if (dropdownwelchestier === 'Katze') {
+        co2EmissionenProTier = 400000; 
+    } else if (dropdownwelchestier === 'Kleintier') {
+        co2EmissionenProTier = 100000;  
+    } else if (dropdownwelchestier === 'Pferd') {
+        co2EmissionenProTier = 3100000;  
+    } else if (dropdownwelchestier === 'Vogel') {
+        co2EmissionenProTier = 25000; 
+    } 
+    co2Emissionen += co2EmissionenProTier * dropdownanzahltiere;
+  }
     }
 
     if (dropdownissocialmedia === "Ja") {
@@ -69,6 +98,20 @@ function co2berechnungperHour(gamingHours, dropdownvalue, platform, dropdownisst
       }
       co2Emissionen += co2EmissionenProNutzung * 7;
     }
+    if (dropdownistiere === "Ja") {
+      if (dropdownwelchestier === 'Hund') {
+        co2EmissionenProTier = 725000; // Co2Emissionen in Gramm auf ein Jahr
+    } else if (dropdownwelchestier === 'Katze') {
+        co2EmissionenProTier = 400000; 
+    } else if (dropdownwelchestier === 'Kleintier') {
+        co2EmissionenProTier = 100000;  
+    } else if (dropdownwelchestier === 'Pferd') {
+        co2EmissionenProTier = 3100000;  
+    } else if (dropdownwelchestier === 'Vogel') {
+        co2EmissionenProTier = 25000; 
+    } 
+    co2Emissionen += co2EmissionenProTier * dropdownanzahltiere;
+  }
     }
 
     if (dropdowniszeitung === "Ja") {
@@ -78,7 +121,36 @@ function co2berechnungperHour(gamingHours, dropdownvalue, platform, dropdownisst
         co2EmissionenProNutzung = 250; 
     }
     co2Emissionen += co2EmissionenProNutzung * 7;
+
+    if (dropdownistiere === "Ja") {
+      if (dropdownwelchestier === 'Hund') {
+        co2EmissionenProTier = 725000; // Co2Emissionen in Gramm auf ein Jahr
+    } else if (dropdownwelchestier === 'Katze') {
+        co2EmissionenProTier = 400000; 
+    } else if (dropdownwelchestier === 'Kleintier') {
+        co2EmissionenProTier = 100000;  
+    } else if (dropdownwelchestier === 'Pferd') {
+        co2EmissionenProTier = 3100000;  
+    } else if (dropdownwelchestier === 'Vogel') {
+        co2EmissionenProTier = 25000; 
+    } 
+    co2Emissionen += co2EmissionenProTier * dropdownanzahltiere;
   }
+  }
+  if (dropdownistiere === "Ja") {
+    if (dropdownwelchestier === 'Hund') {
+      co2EmissionenProTier = 725000; // Co2Emissionen in Gramm auf ein Jahr
+  } else if (dropdownwelchestier === 'Katze') {
+      co2EmissionenProTier = 400000; 
+  } else if (dropdownwelchestier === 'Kleintier') {
+      co2EmissionenProTier = 100000;  
+  } else if (dropdownwelchestier === 'Pferd') {
+      co2EmissionenProTier = 3100000;  
+  } else if (dropdownwelchestier === 'Vogel') {
+      co2EmissionenProTier = 25000; 
+  } 
+  co2Emissionen += co2EmissionenProTier * dropdownanzahltiere;
+}
 
       
   console.log(co2Emissionen);
@@ -98,7 +170,10 @@ document.getElementById('button-berechnen').addEventListener('click', function (
   const dropdowniszeitung = document.getElementById('dropdowniszeitung').value;
   const dropdowniszeitungdp = document.getElementById('dropdowniszeitungdp').value;
   const dropdownistiere = document.getElementById('dropdownistiere').value;
-  const co2berechnung = co2berechnungperHour(gamingHours, dropdownvalue, platform, dropdownisstreaming, streamingHours, dropdownissocialmedia, socialHours, dropdowniszeitung, dropdowniszeitungdp);
+  const dropdownwelchestier = document.getElementById('dropdownwelchestier').value;
+  const dropdownanzahltiere = document.getElementById('dropdownanzahltiere').value;
+  
+  const co2berechnung = co2berechnungperHour(gamingHours, dropdownvalue, platform, dropdownisstreaming, streamingHours, dropdownissocialmedia, socialHours, dropdowniszeitung, dropdowniszeitungdp, dropdownistiere, dropdownwelchestier, dropdownanzahltiere);
   console.log('Geschätzte CO2-Emissionen für die Gaming-Nutzung:', co2berechnung);
   });
 
