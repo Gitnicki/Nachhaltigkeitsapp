092e59eaba35c35816f2c02a54f4fad6dac85567
Marc E. <marc.ehrlich@docc.techstarter.de>
2024-01-08 11:12:01 +0100

    new table

--- sql_scripts/03_isert_data.sql
+++ sql_scripts/03_isert_data.sql
@@ -1,5 +1,7 @@
 USE Co2;
 
-INSERT INTO abdruck(id, benutzername , email, passwort) VALUES (value_1,value_2,value_3);
+INSERT INTO users(benutzername , email, passwort) VALUES (value_1,value_2,value_3);
+
+INSERT INTO abdruck(user_id, kraftstoffausstoß, freizeitverbrauch , wasserverbrauch , stromverbrauch, haushaltverbrauch) VALUES (value_1, value_2, value_3, value_4, value_5, value_6);
 
 
