4c02d1ee4f84438e6a056e63004f6ec95965e472
Sidi Lotfi El Alami <sidi.lotfi@docc.techstarter.de>
2024-01-09 09:40:00 +0100

    add anmelden funktion in login.js

--- login.js
+++ login.js
@@ -1,1 +1,13 @@
-function 
+const apiUrl = "http://localhost:3000/";
+
+function Anmelden(id) {
+    console.log("Benuzer angemeldet ", id);
+    const options = {
+        method: "POST"
+    }
+    fetch(apiUrl + 'login/' + id, options)
+        .then(response => {
+                // getuser();
+            console.log("POST Response", response);
+        })
+}
